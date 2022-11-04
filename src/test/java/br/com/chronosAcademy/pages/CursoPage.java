33c536b8993ffa04edab6a43b7b1153ab0456877
Juliana B Moreira <jubmoreira@gmail.com>
2022-11-04 18:18:50 -0300

    Aula 10 Page Factory

--- src/test/java/br/com/chronosAcademy/pages/CursoPage.java
+++ src/test/java/br/com/chronosAcademy/pages/CursoPage.java
@@ -1,21 +1,22 @@
 package br.com.chronosAcademy.pages;
 
+import br.com.chronosAcademy.maps.CursoMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CursoPage {
     WebDriver driver;
+    CursoMap cursoMap;
 
     public CursoPage(WebDriver driver) {
         this.driver = driver;
+        cursoMap = new CursoMap();
+        PageFactory.initElements(driver,cursoMap);
     }
 
     public String getTitulo2() {
-        String cssTitulo = "div.text-center > div.large-h1";
-        // Ou poderia usar o xPath: //section[4]/div/div/div/div[1]/div/div
-        WebElement txtTitulo = driver.findElement(By.cssSelector(cssTitulo));
-        String titulo = txtTitulo.getText();
-        return titulo;
+        return cursoMap.txtTitulo.getText();
     }
 }
--- src/test/java/br/com/chronosAcademy/pages/PrincipalPage.java
+++ src/test/java/br/com/chronosAcademy/pages/PrincipalPage.java
@@ -1,28 +1,27 @@
 package br.com.chronosAcademy.pages;
 
+import br.com.chronosAcademy.maps.PrincipalMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class PrincipalPage {
-    private WebDriver driver;
+    WebDriver driver;
+    PrincipalMap principalMap;
+
 
     public PrincipalPage(WebDriver driver) {
         this.driver = driver;
+        principalMap = new PrincipalMap();
+        PageFactory.initElements(driver,principalMap);
     }
 
     public String getTitulo() {
-        //section[2]/div[3]/div/div/div/div/div[1]/div/h4 -poderia ser usada sem problema
-        String xpathTitulo = "//section[2]//h4";
-
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
-        String titulo = txtTitulo.getText();
-        return titulo;
+       return principalMap.txtTitulo.getText();
     }
 
     public void clickBotao() {
-        String xpathBotao = "//section[2]/div[3]//a";
-        WebElement btnTitulo = driver.findElement(By.xpath(xpathBotao));
-        btnTitulo.click();
+        principalMap.btnTitulo.click();
     }
 }
