71cbc506d5a9e19ca81cef12297457735f2b0447
Juliana B Moreira <jubmoreira@gmail.com>
2022-11-23 15:43:32 -0300

    Aula 15 Prefixo Pages e Maps

---
+++ src/test/java/br/com/chronosAcademy/maps/LoginMap.java
@@ -0,0 +1,15 @@
+package br.com.chronosAcademy.maps;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+    public WebElement btnLogin;
+    public WebElement bthFechar;
+    public WebElement divFechaModal;
+    public WebElement inpUserName;
+    public WebElement inpPassword;
+    public WebElement inpRemember;
+    public WebElement btnSingIn;
+    public WebElement linkCreateAccount;
+
+}
---
+++ src/test/java/br/com/chronosAcademy/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package br.com.chronosAcademy.pages;
+
+import br.com.chronosAcademy.core.Driver;
+import br.com.chronosAcademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    LoginMap loginMap;
+
+    public LoginPage(){
+        loginMap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+
+    public void clickBtnLogin(){
+        loginMap.btnLogin.click();
+    }
+    public void clickBtnFechar(){
+        loginMap.bthFechar.click();
+    }
+    public void clickDivFecharModal(){
+        loginMap.divFechaModal.click();
+    }
+    public void setInpUserName(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+    public void setInpPassword(String password){
+        loginMap.inpPassword.sendKeys(password);
+    }
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+    public void clickCreateAccount(){
+        loginMap.linkCreateAccount.click();
+    }
+    public void clickBtnSingIn(){
+        loginMap.btnSingIn.click();
+    }
+    public boolean isBtnSingIn(){
+        return loginMap.btnSingIn.isEnabled();
+    }
+
+
+}
